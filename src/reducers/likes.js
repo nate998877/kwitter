61f8864f80f90f9432e458568f7224f2254e4784
Nate <nate998877@gmail.com>
2019-06-10 13:25:34 -0400

    added like and unlike reducers

--- src/reducers/likes.js
+++ src/reducers/likes.js
@@ -1,7 +1,30 @@
+import { LIKE, LIKE_SUCCESS, LIKE_FAIL, UNLIKE, UNLIKE_SUCCESS, UNLIKE_FAIL } from "../actions";
 const initialState = {};
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case LIKE:
+      return {
+        ...state,
+        likeLoading: true,
+        likeError: null
+      };
+    case LIKE_SUCCESS:
+      return { ...state, like: action.payload, likeLoading: false };
+    case LIKE_FAIL:
+      return { ...state, likeError: action.payload, likeLoading: false };
+
+    case UNLIKE:
+      return {
+        ...state,
+        unlikeLoading: true,
+        unlikeError: null
+      };
+    case UNLIKE_SUCCESS:
+      return { ...state, unlike: action.payload, unlikeLoading: false };
+    case UNLIKE_FAIL:
+      return { ...state, unlikeError: action.payload, unlikeLoading: false };
+
     default:
       return state;
   }
