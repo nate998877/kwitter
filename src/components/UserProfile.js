4ee64d6cb377fa621292e93287ce678f58cedf36
Matthew Perry <perry357@outlook.com>
2019-06-10 13:48:27 -0400

    skeleton layout for profile page

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-// import { connect } from "react-redux";
-// import { loginThenGoToUserProfile as login } from "../actions";
+import { connect } from "react-redux";
+import { loginThenGoToUserProfile as login } from "../actions";
 import Spinner from "react-spinkit";
-import { Divider, Grid, Segment } from "semantic-ui-react";
+import settings from "./settingsAcorn.png"
+import { Divider, Button, Grid, Segment } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
+
 
 class UserProfile extends Component {
 
@@ -15,28 +17,36 @@ class UserProfile extends Component {
     const { isLoading, err } = this.props;
     return (
       <React.Fragment>
-        <Segment placeholder>
+        <div id="background">
         {/*NavBar will go here */}
         <Grid columns={1} relaxed='very' doubling centered>
         <Grid.Column>
+
           {/*This is where the picture will go */}
         </Grid.Column>
+        <div id = 'settings'>
+        <Button type="submit" disabled={isLoading}>
+        <img src={settings} alt="new"/>
+        </Button>
+        </div>
         </Grid>
         <Grid columns={3} relaxed='very' doubling centered >
         <Grid.Column>
           {/* This is where the Account information */}
+          rflrfvewrfsd
         </Grid.Column>
         <Grid.Column>
           {/* Posts */}
+          evf efverfdvd
         </Grid.Column>
         <Grid.Column>
           {/* Post information */}
+          vfedfv ev
         </Grid.Column>
         </Grid>
-        <Divider vertical/>
-        </Segment>
         {isLoading && <Spinner name="circle" color="blue" />}
         {err && <p style={{ color: "red" }}>{err}</p>}
+        </div>
       </React.Fragment>
     );
   }
