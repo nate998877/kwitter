655a59290edbcd259a472f132d2819f2c0531aa8
Nate <nate998877@gmail.com>
2019-06-10 15:02:00 -0400

    Implemented users

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,7 +1,37 @@
-const initialState = {};
+import { GET_USERS, GET_USERS_SUCCESS, GET_USERS_FAIL, GET_USER, GET_USER_SUCCESS, GET_USER_FAIL, GET_USER_PHOTO, GET_USER_PHOTO_SUCCESS, GET_USER_PHOTO_FAIL, CREATE_USER, CREATE_USER_SUCCESS, CREATE_USER_FAIL, UPDATE_USER_PHOTO, UPDATE_USER_PHOTO_SUCCESS, UPDATE_USER_PHOTO_FAIL, UPDATE_USER, UPDATE_USER_SUCCESS, UPDATE_USER_FAIL, DELETE_USER, DELETE_USER_SUCCESS, DELETE_USER_FAIL} from "../actions"
+
+
+
+
+const initialState = {
+  usersLoading: false,
+  users: null,
+  usersError: null
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_USERS:
+      return {
+        ...state,
+        getUsersLoading: true,
+        getUsersError: null
+      };
+    case GET_USERS_SUCCESS:
+      return { ...state, getUsers: action.payload, getUsersLoading: false };
+    case GET_USERS_FAIL:
+      return { ...state, getUsersError: action.payload, getUsersLoading: false };
+    case GET_USERS:
+      return {
+        ...state,
+        getUsersLoading: true,
+        getUsersError: null
+      };
+    case GET_USERS_SUCCESS:
+      return { ...state, getUsers: action.payload, getUsersLoading: false };
+    case GET_USERS_FAIL:
+      return { ...state, getUsersError: action.payload, getUsersLoading: false };
+
     default:
       return state;
   }
