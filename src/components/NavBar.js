f27690f22856a7a6c62fb8110a2e792865ec743d
Tamoya Hopkins <tamoyashopkins@gmail.com>
2019-06-11 12:20:51 -0400

    added acorn logo to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Image, Icon } from 'semantic-ui-react'
 import {Route, Switch, Link} from 'react-router-dom'
+import UserProfile from './UserProfile';
+import acorn from './acorn.png'
 
 class NavBar extends Component {
-  state = { activeItem: "Profile" }
+  state = { activeItem: "Acorn Feed" }
 
   handleItemClick = (e, { name }) => {
     this.setState({ activeItem: name })
@@ -11,7 +13,7 @@ class NavBar extends Component {
   }
 
   render() {
-    const { activeItem } = this.state
+    let { activeItem } = this.state
 
     return (
       <div id="navBar-Container">
@@ -22,13 +24,11 @@ class NavBar extends Component {
             active={activeItem === 'Acorn Feed'}
             onClick={this.handleItemClick}
           />
-          <Menu.Item
-            name='New Post'
-            active={activeItem === 'New Post'}
-            onClick={this.handleItemClick}
-          />
+          <Menu.Item name='New Post' active={activeItem === 'New Post'} onClick={this.handleItemClick}>
+            <img src={acorn}/> New Chit
+          </Menu.Item>
           <Menu.Menu position='right'>
-            <Menu.Item name='Profile' href="/profile" active={activeItem === 'Profile'} onClick={this.handleItemClick} />
+            <Menu.Item name='Profile'  href="/profile" active={activeItem === 'Profile'} onClick={this.handleItemClick} />
             <Menu.Item
               name='Logout'
               active={activeItem === 'Logout'}
