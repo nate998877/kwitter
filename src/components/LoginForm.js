019ca5ceab619505dc100797fbc5d42ec34c5f8a
Nate <nate998877@gmail.com>
2019-06-12 12:04:56 -0400

    This branch is dead

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,8 +3,11 @@ import { connect } from "react-redux";
 import { loginThenGoToUserProfile as login } from "../actions";
 import Spinner from "react-spinkit";
 import { Button, Form, Grid } from "semantic-ui-react";
+import Modus from "./Modus"
+import NewUserForm from "./NewUserForm"
 import "semantic-ui-css/semantic.min.css";
 import squirrel from "./Squirrel.png";
+
 
 class LoginForm extends Component {
   state = { username: "", password: "" };
@@ -17,6 +20,10 @@ class LoginForm extends Component {
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
+
+  popUpModus = () =>{
+    this.setState({displayModus:true})
+  }
 
   render() {
     const { isLoading, err } = this.props;
@@ -52,7 +59,7 @@ class LoginForm extends Component {
           <Button type="submit" disabled={isLoading}>
             Forgotten Password
           </Button>
-          <Button type="submit" disabled={isLoading}>
+          <Button type="submit" disabled={isLoading} onClick={()=>{this.popUpModus()}}>
             Create Account
           </Button>
           </div>
@@ -65,6 +72,9 @@ class LoginForm extends Component {
             </div>
             {isLoading && <Spinner name="circle" color="blue" />}
             {err && <p style={{ color: "red" }}>{err}</p>}
+
+        {this.state.displayModus ? <Modus payload={<NewUserForm />}/> : ""}
+        {console.log(this.state)}
       </React.Fragment>
     );
   }
