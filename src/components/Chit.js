bb23568388ea550f426bcac40d2b2ff6877ea53b
Nate <nate998877@gmail.com>
2019-06-17 15:04:45 -0400

    oh no

--- src/components/Chit.js
+++ src/components/Chit.js
@@ -4,9 +4,9 @@ import { Grid, Image } from "semantic-ui-react";
 //for every new post
 //purpose: when new post is needed, and you click on button, this is called and returns template for new post. 
 //takes props: 1)profileImage="urlLink" (will adjust this property's value when we plug in and know how to grab the users uploaded profile pic), 
-//2. userName="string", 3.postContent="string"
+//2. username="string", 3.postContent="string"
 //Also needs to return a post ID (time/data stamp?)
-//example syntax to call Chit: <Chit profileImage="http://3rdpartyservicesofflorida.com/wp-content/uploads/2015/03/blank-profile.jpg" userName="tamoya" postContent="Tamoya's post"></Chit>
+//example syntax to call Chit: <Chit profileImage="http://3rdpartyservicesofflorida.com/wp-content/uploads/2015/03/blank-profile.jpg" username="tamoya" postContent="Tamoya's post"></Chit>
 class Chit extends Component {
     render() {
         return (
@@ -14,11 +14,11 @@ class Chit extends Component {
                 <Grid columns={2} divided>
                     <Grid.Column>
                         <Grid.Row><Image className="userPostProfileImage" src={this.props.profileImage} alt="Profile_Image"></Image></Grid.Row>
-                        <Grid.Row><p>{this.props.userName}</p></Grid.Row>
+                        <Grid.Row><p>{this.props.username}</p></Grid.Row>
                         <Grid.Row><p>{new Date().toDateString()}</p></Grid.Row>
                     </Grid.Column>
                     <Grid.Column>
-                        <p>{this.props.postContent}</p>
+                        <p>{this.props.content}</p>
                     </Grid.Column>
                 </Grid>
             </Grid.Row>
--- src/components/NewsFeedPage.js
+++ src/components/NewsFeedPage.js
@@ -1,36 +1,19 @@
 import React, { Component } from "react";
-import { Grid, Image } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import {NavBar, Chit} from '.'
 
 class NewsFeedPage extends Component {
-    state = {}
-
     render() {
         return (
             <React.Fragment>
-                <Grid.Row>
-                    <NavBar />
-                </Grid.Row>
+                <Grid.Row> <NavBar /> </Grid.Row>
                 <Grid id="newsFeedContainer" columns={3} divided>
-                    <Grid.Column id="left-Col"></Grid.Column>
-                    <Grid.Column id="mid-Col">
-                        <Grid.Row>
-                            <Grid columns={2} divided>
-                                <Grid.Column>
-                                    <Grid.Row><Image class="userPostProfileImage" src="http://3rdpartyservicesofflorida.com/wp-content/uploads/2015/03/blank-profile.jpg"></Image></Grid.Row>
-                                    <Grid.Row><p>UserName</p></Grid.Row>
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <p>#ACRO - "This is my first post.  Just want to say...YOLO!</p>
-                                </Grid.Column>
-                            </Grid>
-                        </Grid.Row>
-                    </Grid.Column>
-                    <Grid.Column id="right-Col"></Grid.Column>
+                    <Grid.Column id="left-Col" />
+                    {/* each post needs two fetches a userphoto and a username fetch this should be handled in a general location */}
+                    <Grid.Column id="mid-Col"> {this.props.posts.map(post => {return( <Chit username={post.id} content={post.text} profileImage={post.image}/>)})} </Grid.Column>
+                    <Grid.Column id="right-Col" />
                 </Grid>
-
             </React.Fragment>
-
         )
     }
 }
